612a18586ada602ff2ec36eead631a34553c2ce2
Nishanth Reddy <Nishanthreddy179@gmail.com>
2025-04-02 08:29:31 +0530

    Added JS_Basics

---
+++ JS_Basics/condition_challenges.js
@@ -0,0 +1,52 @@
+// Checking if a number is greater than another number
+
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+} else {
+  console.log("num2 is greater than num1");
+}
+
+// Checking id a string is equal to another string
+
+let userName = "Nishanth";
+let anotherName = "Nishanth";
+
+if (userName === anotherName) {
+  console.log("username is same as another name");
+} else {
+  console.log("username is not same as another name");
+}
+
+// Checking id a variable is a number or not
+
+let number1 = 15;
+
+if (typeof number1 === "number") {
+  console.log("this is a number");
+} else {
+  console.log("this not is not a number");
+}
+
+// Checking if a boolean value is true or not
+
+let lightOn = true;
+
+if (lightOn) {
+  console.log("yes, the light is on");
+} else {
+  console.log("no, the light is off");
+}
+
+// Checking if Array is empty or not
+
+let myArr = [];
+
+if (myArr.length == 0) {
+    console.log("its an empty array");
+} else {
+    console.log("Somethings inside the array");
+    
+}
