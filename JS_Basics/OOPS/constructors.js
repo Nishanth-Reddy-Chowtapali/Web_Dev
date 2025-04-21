06d60a768da1b644f131263cf02c75352f370a32
Nishanth Reddy <Nishanthreddy179@gmail.com>
2025-04-21 23:32:04 +0530

    added Programs: Dice Roller

---
+++ JS_Basics/OOPS/constructors.js
@@ -0,0 +1,12 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = name;
+}
+
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
+
+let myCar = new Car("Toyota", "Camry");
+console.log(myCar);
