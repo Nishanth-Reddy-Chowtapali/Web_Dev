2674f247f70a4f3f72b162e2ec5a6c96ae0def24
Nishanth Reddy <Nishanthreddy179@gmail.com>
2025-04-23 06:56:53 +0530

    added arrowFunctions

---
+++ JS_Basics/Functions/arrowFunctions.js
@@ -0,0 +1,9 @@
+const hello = () => console.log("Hello");
+
+const message = (name, age) => {
+  console.log(`Hello ${name}`);
+  console.log(`You are ${age} years old`);
+};
+
+hello();
+message("Nishanth", 28);
